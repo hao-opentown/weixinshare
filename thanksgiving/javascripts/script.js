d608be4bae750cd6348df3c28ba115e443e19aab
zhihao wang <hao@opentown.cn>
2015-11-24 23:36:15 +0800

    add js sdk

--- thanksgiving/javascripts/script.js
+++ thanksgiving/javascripts/script.js
@@ -5,7 +5,7 @@
           event.preventDefault();
           mockAction();
       });
-      $('.share-bar .fa').on('click',function() {
+      $('.share-bar .fa').on('click', function() {
           $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
               function() {
                   $(this).removeClass('animated pulse');
@@ -34,4 +34,51 @@
           $(".page_8 .card").attr("src", link);
 
       }
+
+      wechatInit();
+
+      function wechatInit() {
+
+
+          // wechat sdk 签名
+          var wxReq = $.ajax({
+              url: 'http://opentown.cn/wechat/rsx/0',
+              type: 'post',
+              dataType: 'json',
+              data: {
+                  url: location.href.split('#')[0]
+              }
+          }).done(function(resp) {
+              var r = resp;
+              wx.config({
+                  appId: r.appid,
+                  debug: true,
+                  timestamp: r.timestamp,
+                  nonceStr: r.nonceStr,
+                  signature: r.signature,
+                  jsApiList: [
+                      'checkJsApi',
+                      'onMenuShareTimeline',
+                      'onMenuShareAppMessage',
+                      'onMenuShareQQ',
+                      'onMenuShareWeibo',
+                      'chooseImage'
+                  ]
+              });
+              // 调用微信API
+              wx.ready(function() {
+                  var txt = "hi";
+                  var sdata = {
+                      title: txt,
+                      desc: txt,
+                      link: location.href.split('#')[0],
+                      imgUrl: 'http://game.4gshu.com/xuangedan/other-project/2015-01-06/img/share-wx-logo.jpg',
+                  };
+                  // wx.onMenuShareTimeline(sdata);
+                  wx.onMenuShareAppMessage(sdata);
+              });
+          });
+
+      }
+
   });
